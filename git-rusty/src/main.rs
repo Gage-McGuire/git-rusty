2c95e4f69b267f5f0b57d7149fc0171621462bf2
Gage McGuire <72943845+Gage-McGuire@users.noreply.github.com>
2024-12-19 16:35:59 -0600

    get user input command

--- git-rusty/src/main.rs
+++ git-rusty/src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("HEYYYYY LETS GET RUSTY");
+    let mut input = String::new();
+    print!("Enter a command: ");
+    let _ = io::Write::flush(&mut io::stdout());
+    let _ = io::stdin().read_line(&mut input);
+    println!("You entered: {}", input);
 }
