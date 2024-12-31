5f994d62ab3423d4d03b3866f829c598b7110777
Gage McGuire <72943845+Gage-McGuire@users.noreply.github.com>
2024-12-30 22:35:53 -0600

    init cmd to create .git-rusty file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod git_rusty;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Enter a command: ");
+    std::io::Write::flush(&mut std::io::stdout()).expect("flush failed!");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let command = input;
+    match command.trim() {
+        "init" => git_rusty::init(),
+        "cat-file" => git_rusty::cat_file(),
+        _ => println!("Unknown command: {}", command),
+    }
 }
